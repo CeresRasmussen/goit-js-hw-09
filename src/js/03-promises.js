a7376f1b296dcecd1519ebe469eec9f5c1595541
Ceres Rasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2023-02-27 00:27:10 +0200

    Update 03-promises.js
    Hate Timer

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import Notiflix from 'notiflix';
+const refs = {
+  dataForm: document.querySelector('.form'),
+  firstDelayInput: document.querySelector('input[name="delay"]'),
+  delayStepInput: document.querySelector('input[name="step"]'),
+  amountStep: document.querySelector('input[name="amount"]'),
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const shouldResolve = Math.random() > 0.5;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        reject({ position, delay });
+      }
+      resolve({ position, delay });
+    }, delay);
+  });
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  const amount = refs.amountStep.value;
+  let delay = Number(refs.firstDelayInput.value);
+  let stepDelay = Number(refs.delayStepInput.value);
+
+  for (let index = 1; index <= amount; index += 1) {
+    createPromise(index, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += stepDelay;
   }
 }
+refs.dataForm.addEventListener('submit', onSubmit, { once: true });
